6a79640f16ae8201e0d4d86bf562f3cd5d3845cf
DanielRSnell <hi@danielsnell.ninja>
2018-10-06 16:37:05 -0400

    React Router

--- src/App.js
+++ src/App.js
@@ -1,8 +1,13 @@
 import React, {Component} from 'react';
 import {connect} from 'react-redux';
+import {BrowserRouter as Router, Route} from "react-router-dom";
+
 import logo from './logo.svg';
 import './App.css';
 import {simpleAction} from './actions/simpleAction.js'
+
+// Containers
+import Page from './components/containers/page'
 
 class App extends Component {
     simpleAction = (event) => {
@@ -11,21 +16,24 @@ class App extends Component {
 
     render() {
         return (
-            <div className="App">
-                <header className="App-header">
-                    <img src={logo} className="App-logo" alt="logo"/>
-                    <h1 className="App-title">Welcome to React</h1>
-                </header>
-                <p className="App-intro">
-                    To get started, edit <code>src/App.js</code> and save to reload
-                </p>
-                <pre>
- {
-     JSON.stringify(this.props)
- }
-</pre>
-                <button onClick={this.simpleAction}>Test redux action</button>
-            </div>
+            <Router>
+                <div className="App">
+                    <header className="App-header">
+                        <img src={logo} className="App-logo" alt="logo"/>
+                        <h1 className="App-title">Welcome to React</h1>
+                    </header>
+                    <p className="App-intro">
+                        To get started, edit <code>src/App.js</code> and save to reload
+                    </p>
+                    <pre>
+                 {
+                     JSON.stringify(this.props)
+                 }
+                </pre>
+                    <button onClick={this.simpleAction}>Test redux action</button>
+                    <Route path={"/page"} component={Page}/>
+                </div>
+            </Router>
         );
     }
 }
---
+++ src/components/containers/page.js
@@ -0,0 +1,14 @@
+import React, {Component} from 'react'
+
+class Page extends Component {
+    render() {
+        return (
+            <div className={"container"}>
+                <h1>This is just a page</h1>
+                <h2>Nothing Magical Here...(Yet!)</h2>
+            </div>
+        )
+    }
+}
+
+export default Page;
